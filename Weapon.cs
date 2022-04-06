05ee0b03b040cdc256a0debd2d2c5d851b121e8f
soGloomyCat <tonyhowk001@gmail.com>
2022-04-06 18:44:53 +0300

    Create Weapon.cs

---
+++ Weapon.cs
@@ -0,0 +1,35 @@
+﻿namespace Weapon
+{
+    class Weapon
+    {
+        private int _bullets;
+
+        public int Damage { get; }
+
+        public void Fire()
+        {
+            _bullets--;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire();
+            player.TakeDamage(_weapon.Damage);
+        }
+    }
+}
