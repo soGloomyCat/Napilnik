3caf6caa3d1f214de62a562b7a0cebabb4286e0a
soGloomyCat <tonyhowk001@gmail.com>
2022-04-10 21:50:40 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PathFinder ConsolePathfinder = new PathFinder(new ConsoleLogWritter());
+            PathFinder FilePathfinder1 = new PathFinder(new FileLogWritter());
+            PathFinder SecureConsolePathfinder = new PathFinder(new SecureConsoleLogWritter());
+            PathFinder SecureFilePathfinder1 = new PathFinder(new SecureFileLogWritter());
+            PathFinder SecureMultiPathfinder1 = new PathFinder(new SecureMultiLogWritter());
+
+            ConsolePathfinder.Find();
+            FilePathfinder1.Find();
+            SecureConsolePathfinder.Find();
+            SecureFilePathfinder1.Find();
+            SecureMultiPathfinder1.Find();
+
+        }
+    }
+
+    class PathFinder
+    {
+        private ILogger _logger;
+
+        public PathFinder(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Find()
+        {
+            _logger.WriteMessage();
+        }
+    }
+
+    class LogWritter : ILogger
+    {
+        private string _message;
+
+        public string Message => _message;
+        public bool IsNeedDay => CheckDayOfTheWeek();
+
+        public LogWritter()
+        {
+            _message = GetMessage();
+        }
+
+        public virtual void WriteMessage()
+        {
+        }
+
+        protected void OutputInformationalMessage()
+        {
+            Console.WriteLine("Лог не сгенерирован, т.к. генерация производится в другой день.");
+        }
+
+        private bool CheckDayOfTheWeek()
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+                return true;
+            else
+                return false;
+        }
+
+        private string GetMessage()
+        {
+            Console.Write("Установите сообщение для логгера: ");
+            return _message = Console.ReadLine();
+        }
+    }
+
+    class ConsoleLogWritter : LogWritter, IConsoleLogger
+    {
+        public override void WriteMessage()
+        {
+            WriteConsoleMessage(Message);
+        }
+
+        public void WriteConsoleMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter : LogWritter, IFileLogger
+    {
+        public override void WriteMessage()
+        {
+            WriteFileMessage(Message);
+        }
+
+        public void WriteFileMessage(string message)
+        {
+            File.WriteAllText("log0.txt", message);
+            Console.WriteLine("Файл сгенерирован");
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteMessage()
+        {
+            if (IsNeedDay)
+                WriteConsoleMessage(Message);
+            else
+                OutputInformationalMessage();
+        }
+    }
+
+    class SecureFileLogWritter : LogWritter, IFileLogger
+    {
+        public override void WriteMessage()
+        {
+            if (IsNeedDay)
+                WriteFileMessage(Message);
+            else
+                OutputInformationalMessage();
+        }
+
+        public void WriteFileMessage(string message)
+        {
+            File.WriteAllText("log1.txt", message);
+            Console.WriteLine("Файл сгенерирован");
+        }
+    }
+
+    class SecureMultiLogWritter : ConsoleLogWritter, IFileLogger
+    {
+        public override void WriteMessage()
+        {
+            WriteConsoleMessage(Message);
+
+            if (IsNeedDay)
+                WriteFileMessage(Message);
+            else
+                OutputInformationalMessage();
+        }
+
+        public void WriteFileMessage(string message)
+        {
+            File.WriteAllText("log2.txt", message);
+            Console.WriteLine("Файл сгенерирован");
+        }
+    }
+
+    interface ILogger
+    {
+        string Message { get; }
+        void WriteMessage();
+    }
+
+    interface IConsoleLogger : ILogger
+    {
+        void WriteConsoleMessage(string message);
+    }
+
+    interface IFileLogger : ILogger
+    {
+        void WriteFileMessage(string message);
+    }
+}
